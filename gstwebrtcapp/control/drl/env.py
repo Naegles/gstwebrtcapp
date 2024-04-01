9a04406bbffd275e487437e9ce3524845788a28d
Naegles <44474684+Naegles@users.noreply.github.com>
2024-04-02 00:29:48 +0200

    Update env.py

--- gstwebrtcapp/control/drl/env.py
+++ gstwebrtcapp/control/drl/env.py
@@ -281,6 +281,8 @@ class FedEnv(Env):
         if not self.mdp.is_deliver_all_observations:
             # 15% of the observations are selected to be cut to prevent the influence of the last action
             obs_list = select_n_equidistant_elements_from_list(obs_list, self.mdp.num_observations_for_state, 15)
+        else:
+            obs_list = cut_first_elements_in_list(obs_list, 15, self.mdp.num_observations_for_state)
         final_obs_dict = merge_observations(obs_list) if len(obs_list) > 1 else obs_list[0]
         return final_obs_dict
 
